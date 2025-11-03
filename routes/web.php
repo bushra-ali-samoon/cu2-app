4ea78cf4f635e19d83030b2cae97c2f0045f9e9c
Bushra Ali <99665258+bushra-ali-samoon@users.noreply.github.com>
2025-11-03 10:03:40 +0500

    Added new theme and updated dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+Route::get('/admin', function () {
+    return 'Welcome Admin!';
+})->middleware('role:admin');
